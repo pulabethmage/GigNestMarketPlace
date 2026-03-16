ac9ef15ca385bcc1c2c1578ee87a7c034bb6297d
Pulasthi Bethmage <pulasthibethmage@gmail.com>
2026-03-16 14:04:09 +0530

    Footer Text Updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center py-8 bg-green-800 text-white">
-      <p>© 2026 GigNest. All rights reserved.</p>
+      <p>© 2026 GigNest PVT Ltd. All rights reserved.</p>
     </footer>
   );
 }
